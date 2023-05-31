617dfacfcae7b7cca506cc4eff6a38d7cc5cd62f
Nitan Shalon <nitanshalon@gmail.com>
2023-05-31 13:16:15 -0700

    update

--- train.py
+++ train.py
@@ -175,8 +175,15 @@ class DataCollatorForSupervisedDataset(object):
 
 def make_supervised_data_module(tokenizer: transformers.PreTrainedTokenizer, data_args) -> Dict:
     """Make dataset and collator for supervised fine-tuning."""
-    train_dataset = SupervisedDataset(tokenizer=tokenizer, data_path=data_args.data_path)
-    data_collator = DataCollatorForSupervisedDataset(tokenizer=tokenizer)
+    
+    train_dataset = SupervisedDataset(
+        tokenizer=tokenizer, 
+        data_path=data_args.data_path
+    )
+    
+    data_collator = DataCollatorForSupervisedDataset(
+        tokenizer=tokenizer
+    )
     
     return dict(
         train_dataset=train_dataset, 
